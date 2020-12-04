eaa87ac97aafe1f67f8ec12591487cbd16f1b72c
tomo_joz <tomoya.shozu@gmail.com>
2020-12-04 16:34:40 +0900

    user.rb にてタイポ修正しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,5 +22,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :articles, dependent :destroy
+  has_many :articles, dependent: :destroy
 end
